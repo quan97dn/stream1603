7e01d465235b25bcb56014e12ca9edb0d3d4e315
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-13 18:29:39 +0700

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+click -> make a call
+    step 1: open the Camera
+    step 2: create a new peer and wait for signal
+    step 3: send the signal to node server
+    step 4: listen for other's signal
+    step 5: on stream function
+    step 6: open the call
---
+++ src/app.js
@@ -0,0 +1,32 @@
+const $ = require('jquery');
+const io = require('socket.io-client');
+
+$('document').ready(() => {
+    const socket = io();
+    $('#divChat').hide();
+
+    $('#btnSignUp').click(() => {
+        const username = $('#txtUsername').val();
+        socket.emit('DANG_KY_USERNAME', username);
+    });
+
+    socket.on('XAC_NHAN_DANG_KY', arrUser => {
+        if (arrUser) {
+            arrUser.forEach(e => {
+                $('#ulUser').append(`<li id="${e}">${e}</li>`);
+            });
+            socket.on('NGUOI_DUNG_MOI', username => {
+                $('#ulUser').append(`<li id="${username}">${username}</li>`);
+            });
+            $('#divChat').show();
+            return $('#divSignUp').hide();
+        }
+        alert('Username da ton tai!');
+    });
+
+    socket.on('NGUOI_DUNG_THOAT', username => {
+        $(`#${username}`).remove();
+    });
+});
+
+//https://socket.io/docs/emit-cheatsheet/
